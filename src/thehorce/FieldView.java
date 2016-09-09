e932155b48afd55234059541a4b6f93d143dc1cf
sv_kopylov <imber81@gmail.com>
2016-09-09 20:18:25 +0300

    finite 0 vers

---
+++ src/thehorce/FieldView.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thehorce;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Сергей
+ */
+public class FieldView {
+   private final FieldModel modelField;
+   private final Button buttons[][];
+   private final GridPane grid;
+   private final ControllerHandler controllerHandler;
+   private final Scene scene;
+   
+   public Scene getScene (){
+       return scene;
+   }
+
+    public FieldView(FieldModel modelField, 
+            ControllerHandler controllerHandler) {
+        this.modelField = modelField;
+        this.controllerHandler = controllerHandler;
+        int size = modelField.fieldSize;
+        buttons = new Button[size][size];
+        grid = new GridPane();
+        
+        
+        for (int i=0; i<size;i++){
+            for (int j=0; j<size; j++){
+                buttons[i][j]=new Button();
+                buttons[i][j].disableProperty().bind(modelField.getCell(i, j).disabled);
+                buttons[i][j].setId(Integer.toString(i)+"%"+Integer.toString(j));
+              
+                buttons[i][j].setOnAction(controllerHandler);
+                buttons[i][j].setMinSize(40, 40);
+                grid.add(buttons[i][j], i, j);
+            }
+            
+          
+        }
+          scene = new Scene(grid, modelField.fieldSize*40, modelField.fieldSize*40);
+        
+        
+    }
+    
+    
+}
